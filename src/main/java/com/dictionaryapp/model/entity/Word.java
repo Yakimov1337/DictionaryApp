bdcaa90a4b2acdddab6969de48508c35bd53479a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-06-20 11:57:56 +0300

    create word entity

---
+++ src/main/java/com/dictionaryapp/model/entity/Word.java
@@ -0,0 +1,98 @@
+package com.dictionaryapp.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "words")
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String term;
+
+    @Column(nullable = false)
+    private String translation;
+
+    private String example;
+
+    @Column(nullable = false)
+    private Instant date;
+
+    @ManyToOne(optional = false)
+//    @JoinColumn(name = "languages_id", nullable = false)
+    private Language language;
+
+    @ManyToOne()
+//    @JoinColumn(name = "user_id")
+    private User addedBy;
+
+    public Word() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public User getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(User addedBy) {
+        this.addedBy = addedBy;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public User getUser() {
+        return addedBy;
+    }
+
+    public void setUser(User user) {
+        this.addedBy = user;
+    }
+}
