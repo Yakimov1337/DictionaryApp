e3d41e21cf947f082aa28376f49bc7630b19d87c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-06-21 09:18:19 +0300

    Create AddWordDTO.java

---
+++ src/main/java/com/dictionaryapp/model/dto/AddWordDTO.java
@@ -0,0 +1,15 @@
+package com.dictionaryapp.model.dto;
+
+import java.time.Instant;
+
+public class AddWordDTO {
+    private String term;
+
+    private String translation;
+
+    private String example;
+
+    private Instant created;
+
+    private String language;
+}
