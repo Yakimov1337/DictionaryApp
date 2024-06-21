c74ede8aadd1b16d0f133fd11bc4f44899fef69a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-06-21 10:50:28 +0300

    Update AddWordDTO.java

--- src/main/java/com/dictionaryapp/model/dto/AddWordDTO.java
+++ src/main/java/com/dictionaryapp/model/dto/AddWordDTO.java
@@ -1,6 +1,6 @@
 package com.dictionaryapp.model.dto;
 
-import java.time.Instant;
+import java.time.LocalDate;
 
 public class AddWordDTO {
     private String term;
@@ -9,7 +9,49 @@ public class AddWordDTO {
 
     private String example;
 
-    private Instant created;
+    private LocalDate inputDate;
 
     private String language;
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public LocalDate getInputDate() {
+        return inputDate;
+    }
+
+    public void setInputDate(LocalDate inputDate) {
+        this.inputDate = inputDate;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public AddWordDTO() {}
 }
